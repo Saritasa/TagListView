303687dfa06a36aa969fb8559011752005c3a681
Alex True <troohin@saritasa.com>
2016-12-17 16:05:44 +0700

    Add public modifier to textFont property of TagView

--- TagListView/TagView.swift
+++ TagListView/TagView.swift
@@ -76,7 +76,7 @@ open class TagView: UIButton {
         }
     }
     
-    var textFont: UIFont = UIFont.systemFont(ofSize: 12) {
+    public var textFont: UIFont = UIFont.systemFont(ofSize: 12) {
         didSet {
             titleLabel?.font = textFont
         }
